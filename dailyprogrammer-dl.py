0a10f6d5a4cbb33b053037d619dfa21066d0c8e3
David Maranhao <uberl33thax0r@hotmail.com>
2015-08-17 23:14:46 -0400

    Clean up, stop using local test file as input

--- dailyprogrammer-dl.py
+++ dailyprogrammer-dl.py
@@ -23,17 +23,11 @@ Options:
 
 from docopt import docopt
 from robobrowser import RoboBrowser
-import urllib
 import logging
 import re
 import shutil
 import os
-import glob
-import shlex
-import subprocess
-
 import html2text
-from bs4 import BeautifulSoup
 
 
 __version__="0.1.0" # MAJOR.MINOR.PATCH | http://semver.org
@@ -56,29 +50,15 @@ def main():
 
     # Parse project page for title and description
     logging.info("Parsing daily challenge: {}".format(challengeURL))
-    #browser = RoboBrowser()
-    #browser.open(challengeURL)
-    #title = browser.find('a',class_='title').string
-    #description = browser.find('div',class_="md")
-
-    soup = BeautifulSoup(open('test.html'))
-    title = soup.find('a',class_='title').string
-    description = soup.find_all('div',class_="md")
+    browser = RoboBrowser()
+    browser.session.headers['User-Agent'] = "dailyprogrammer-dl v{}".format(__version__)
+    browser.open(challengeURL)
+    title = browser.find('a',class_='title').string
+    description = browser.find_all('div',class_="md")
     description = description[1]
     descriptionHTML = "".join(str(t) for t in description.contents) # remove outer <div>
 
-    print(title)
-    print(descriptionHTML)
-    #print(description)
-    #print(description)
-    #print(description)
-
     projectName = generateProjectName(title)
-    print(projectName)
-
-    h = html2text.HTML2Text()
-    descriptionMD = h.handle(descriptionHTML)
-    print(descriptionMD)
 
     # Init project skeleton
     logging.info("Generating project")
@@ -89,24 +69,12 @@ def main():
     pyTemplate = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)),"boilerplate.txt"))
     shutil.copy(pyTemplate, os.path.join(projectPath,"{}.py".format(projectName)))
 
+    # Generate README.md
+    h = html2text.HTML2Text()
+    descriptionMD = h.handle(descriptionHTML)
     readme = os.path.join(projectPath,"README.md")
     with open(readme, "w") as f:
         f.write(descriptionMD)
-
-    ## Initialize git repo
-    #shellCommand("git init",cwd=projectPath)
-    #shellCommand("git add {}-src.txt".format(projectName),cwd=projectPath)
-    #shellCommand('git commit -m "dailyprogrammer-dl: Initial version"',cwd=projectPath)
-    #
-    ## Convert to UTF-8
-    #shellCommand("recode ISO-8859-1..UTF-8 {}".format(os.path.abspath("{}/{}-src.txt".format(projectName,projectName))))
-    #prependText('', projectFilePath) # convert to native line endings
-    #shellCommand('git commit -am "dailyprogrammer-dl: Convert to UTF-8, native line endings"',cwd=projectPath)
-    #
-    ## Add title
-    #titleTxt = ".dt {}, by {}—A Project Gutenberg eBook\n".format(title,author)
-    #prependText(titleTxt, projectFilePath)
-    #shellCommand('git commit -am "dailyprogrammer-dl: Add title"',cwd=projectPath)
 
     return
 
@@ -117,33 +85,6 @@ def prependText( s, fn ):
     with open(fn,'w') as f:
         f.write(s)
         f.write(t)
-
-
-def shellCommand( s, cwd=None ):
-    logging.info("Executing shell command: {}".format(s))
-    cl = shlex.split(s)
-    proc=subprocess.Popen(cl,cwd=cwd)
-    proc.wait()
-    if( proc.returncode != 0 ):
-        logging.error("Command failed: {}".format(s))
-
-
-def moveFiles( files, dest ):
-    for f in files:
-        logging.info("Moving file {} to {}".format(f,dest))
-        shutil.move(f,dest)
-
-
-def downloadFile( src, dest ):
-    logging.info("Saving {} to {}".format(src,dest))
-    try:
-        urllib.request.urlretrieve(src,dest)
-    except urllib.error.HTTPError as e:
-        if e.code == 404:
-            logging.error("Error downloading {}\nHTTP 404: Not Found {}".format(dest,src))
-            pass
-        else:
-            raise
 
 
 def generateProjectName( s ):
